bfbac3a48dfbdbf6111ff02cca28398028311ac5
unknown <ehddn>
2021-05-06 19:47:38 +0900

    nav

---
+++ justvote/src/components/SubbarItems.js
@@ -0,0 +1,38 @@
+export const SubbarItems = [
+    {
+        title: '모두',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: '연예',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: '시사',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: 'TV',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: '학교',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: '썰',
+        url: '#',
+        cName: 'sub-links'
+    },
+    {
+        title: '기타',
+        url: '#',
+        cName: 'sub-links'
+    },
+]
+
