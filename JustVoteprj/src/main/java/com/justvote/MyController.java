c2231400339a260192da441ef5d25865c6e3c24d
josomang <whthakd1207@naver.com>
2021-05-26 00:34:28 +0900

    prj

--- JustVoteprj/src/main/java/com/justvote/MyController.java
+++ JustVoteprj/src/main/java/com/justvote/MyController.java
@@ -2,100 +2,23 @@ package com.justvote;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.justvote.dao.IRegisterDao;
+import com.justvote.dto.RegisterDto;
 
-class Person {
-
-	public String getUserName() {
-		return userName;
-	}
-
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-
-	public String getSex() {
-		return sex;
-	}
-
-	public void setSex(String sex) {
-		this.sex = sex;
-	}
-
-	public String getAge() {
-		return age;
-	}
-
-	public void setAge(String age) {
-		this.age = age;
-	}
-
-	public String getMajor() {
-		return major;
-	}
-
-	public void setMajor(String major) {
-		this.major = major;
-	}
-
-	public String getGrade() {
-		return grade;
-	}
-
-	public void setGrade(String grade) {
-		this.grade = grade;
-	}
-
-	public String getNickName() {
-		return nickName;
-	}
-
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-
-	public String getUserId() {
-		return userId;
-	}
-
-	public void setUserId(String userId) {
-		this.userId = userId;
-	}
-
-	public String getUserPass() {
-		return userPass;
-	}
-
-	public void setUserPass(String userPass) {
-		this.userPass = userPass;
-	}
-
-	@Override
-	public String toString() {
-		return "Person [userName=" + userName + ", sex=" + sex + ", age=" + age + ", major=" + major + ", grade="
-				+ grade + ", nickName=" + nickName + ", userId=" + userId + ", userPass=" + userPass + "]";
-	}
-
-	public String userName; // client Name
-	public String sex; // client sex
-	public String age; // client age
-	public String major; // client major
-	public String grade; // client grade
-	public String nickName; // client nickName
-	public String userId; // client
-	public String userPass; // client PW
-}
+	
 
 @CrossOrigin(origins = "http://localhost:3000")
 @RestController
 public class MyController {
 	@Autowired
 	IRegisterDao dao;
+	
 
 	@RequestMapping("/")
 	public String root() throws Exception {
@@ -105,12 +28,26 @@ public class MyController {
 	}
 
 	@PostMapping("/register")
-	public String write(@RequestBody Person person) {
-		dao.registerDao(person.getUserName(), person.getSex(), person.getAge(), person.getMajor(), person.getGrade(),
-				person.getNickName(), person.getUserId(), person.getUserPass());
+	public String write(@RequestBody RegisterDto registerDto) {
+		dao.registerDao(registerDto.getUserName(), registerDto.getSex(), registerDto.getAge(), registerDto.getMajor(),
+				registerDto.getGrade(), registerDto.getNickName(), registerDto.getUserId(), registerDto.getUserPass());
 
-		return person.toString();
+		return registerDto.toString();
 
 	}
+	@PostMapping("/signin")
+	public String login(@RequestBody RegisterDto loginDto) {
+		dao.loginDao(loginDto.getUserId(), loginDto.getUserPass());
+		
+		return loginDto.toString(); 
+		
+	}
+	@GetMapping("/getInfo")
+	public String checkInfo(@RequestBody RegisterDto checkDto) {
+	dao.check(checkDto.getUserId());
+	return checkDto.toString();
+		
+	}
+	
 
 }
